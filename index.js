97bca7c958c053afe5a6e63fd3b9c75bfae23518
ajeet.manand <ajeet.manand@wu.com>
2018-07-04 02:36:04 +0530

    updated

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var http = require('http');
 
 const PORT = 80;
 
-function requestHandler(req, res){
-	res.end('Hello ${process.platform}');
+function requestHandler(req, res) {
+	res.end(`Hello ${process.platform}`);
 }
 
 var server = http.createServer(requestHandler);
 
 server.listen(PORT, function(){
-	console.log('${process.env.NODE_ENV} server listinig on port: ${PORT} Crtl+C for exit');
+	console.log(`${process.env.NODE_ENV} server listening on port: ${PORT}. CTRL-C to exit.`);
 });
